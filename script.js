a82bbad0436741c1c9e30a7b7e0e412309ded291
RiviCodes <jose.riveramz@udlap.mx>
2022-01-28 15:34:42 -0600

    Add basic functions of the calculator: addition, subtraction, multiplication and divition. Initiate operate function

---
+++ script.js
@@ -0,0 +1,28 @@
+// Operate function
+const operate = (numX, numY, operator) => {
+
+  if (operator == '+') {
+    return add(numX, numY);
+  } else if (operator == '-') {
+    return subtract(numX, numY);
+  } else if (operator == '*') {
+    return multiply(numX, numY);
+  }
+};
+
+// Basic math operations
+const add = (x, y) => {
+  return x + y;
+};
+
+const subtract = (x, y) => {
+  return x - y;
+};
+
+const multiply = (x, y) => {
+  return x * y;
+};
+
+const divide = (x, y) => {
+  return x / y;
+};
